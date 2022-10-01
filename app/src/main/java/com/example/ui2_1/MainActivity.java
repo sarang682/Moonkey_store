d9e66b6b6a68f45d112738a3889a4d8b0459d4c7
DESKTOP-3UBUOEG\사랑 <jms20914@gmail.com>
2022-10-01 18:23:56 +0900

    점주 프로젝트 레이아웃 파일 커밋

---
+++ app/src/main/java/com/example/ui2_1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.ui2_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.fragment_home);
+
+    }
+}
