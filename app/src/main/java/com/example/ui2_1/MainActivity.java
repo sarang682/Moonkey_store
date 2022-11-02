c0cd526a95a3c1c107635721cd3460c704e27e35
sujin <sujin8239@naver.com>
2022-11-02 12:48:03 +0900

    마이페이지,회원정보수정

--- app/src/main/java/com/example/ui2_1/MainActivity.java
+++ app/src/main/java/com/example/ui2_1/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.ui2_1;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.widget.NestedScrollView;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
@@ -27,18 +29,32 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<Menu> items = new ArrayList<Menu>();
     private NestedScrollView nestedScrollView;
 
-    private ImageView openDrawer;
+    private ImageView openDrawer, mypage;
     private Button logout;
-    private LinearLayout review, orderlist, standing, question;
+    private LinearLayout review, orderlist, standing, question, editAccnt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.fragment_home);
 
-        //상품 추가
+
+        mypage=findViewById(R.id.mypage);
         btn_add=findViewById(R.id.imageView2);
+        listView=findViewById(R.id.listView);
+        nestedScrollView=findViewById(R.id.nested_scroll_view);
+
+        //마이페이지
+        mypage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),MyPageActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        //상품 추가
         btn_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -48,14 +64,12 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //리스트뷰
-        listView=findViewById(R.id.listView);
         adapter=new MenuAdapter(items);
         items.add(new Menu("10000원","떡볶이","밀떡으로 만들었어용"));
         items.add(new Menu("21000원","로제떡볶이","쌀떡으로 만들었어용"));
         listView.setAdapter(adapter);
 
         //
-        nestedScrollView=findViewById(R.id.nested_scroll_view);
         listView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -65,7 +79,9 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        //슬라이드 메뉴
+        //////////////////////////////////////////////////
+        // 슬라이드 메뉴
+
         final DrawerLayout drawerLayout= (DrawerLayout) findViewById(R.id.drawerLayout);
         final View drawerView =findViewById(R.id.ll_drawer);
         openDrawer=findViewById(R.id.iv_hamburger);
@@ -81,11 +97,19 @@ public class MainActivity extends AppCompatActivity {
         review=findViewById(R.id.review);
         orderlist=findViewById(R.id.orderlist);
         standing=findViewById(R.id.standing);
-
         question=findViewById(R.id.question);
-
         logout=findViewById(R.id.btnLogout);
+        editAccnt=findViewById(R.id.linearLayout5);
 
+
+        //회원정보수정
+        editAccnt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, EditAccountActivity.class);
+                startActivity(intent);
+            }
+        });
 
         //리뷰관리
         review.setOnClickListener(new View.OnClickListener() {
@@ -116,21 +140,31 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //로그아웃
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
-
         //문의사항
         question.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, QuestionActivity.class);
                 startActivity(intent);
+            }
+        });
+
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("로그아웃하시겠습니까?");
+                builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                    }
+                });
+                builder.setNeutralButton("아니오",null);
+                builder.create().show();
+
             }
         });
 
